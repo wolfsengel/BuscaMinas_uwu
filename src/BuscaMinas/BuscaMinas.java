8202bffacf835b5ecb8d47c9f151e15ffa99be11
angel <angel.gomez.prol@udc.es>
2023-02-22 19:10:17 +0100

    Final Commit

--- src/BuscaMinas/BuscaMinas.java
+++ src/BuscaMinas/BuscaMinas.java
@@ -1,4 +1,85 @@
 package BuscaMinas;
 
+import java.util.Random;
+
 public class BuscaMinas {
+    private final int FILAS = 8;
+    private final int COLUMNAS = 8;
+    private final int MINAS = 10;
+
+    public final char[][] tablero = new char[FILAS][COLUMNAS];
+    public final boolean[][] minas = new boolean[FILAS][COLUMNAS];
+    public final boolean[][] descubiertas = new boolean[FILAS][COLUMNAS];
+    private final Random random = new Random();
+
+    public void inicializarTablero() {
+        for (int i = 0; i < FILAS; i++) {
+            for (int j = 0; j < COLUMNAS; j++) {
+                tablero[i][j] = ' ';
+                minas[i][j] = false;
+                descubiertas[i][j] = false;
+            }
+        }
+    }
+    public void colocarMinas() {
+        int minasColocadas = 0;
+
+        while (minasColocadas < MINAS) {
+            int fila = random.nextInt(FILAS);
+            int columna = random.nextInt(COLUMNAS);
+
+            if (!minas[fila][columna]) {
+                minas[fila][columna] = true;
+                minasColocadas++;
+            }
+        }
+    }
+    public boolean esMina(int fila, int columna) {
+        return minas[fila][columna];
+    }
+    public void descubrirCasilla(int fila, int columna) {
+        if (!descubiertas[fila][columna]) {
+            descubiertas[fila][columna] = true;
+
+            if (minas[fila][columna]) {
+                return;
+            }
+
+            if (contarMinasCercanas(fila, columna) == 0) {
+                for (int i = Math.max(fila - 1, 0); i <= Math.min(fila + 1, FILAS - 1); i++) {
+                    for (int j = Math.max(columna - 1, 0); j <= Math.min(columna + 1, COLUMNAS - 1); j++) {
+                        if (i != fila || j != columna) {
+                            descubrirCasilla(i, j);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    public int contarMinasCercanas(int fila, int columna) {
+        int minasCercanas = 0;
+
+        for (int i = Math.max(fila - 1, 0); i <= Math.min(fila + 1, FILAS - 1); i++) {
+            for (int j = Math.max(columna - 1, 0); j <= Math.min(columna + 1, COLUMNAS - 1); j++) {
+                if (minas[i][j]) {
+                    minasCercanas++;
+                }
+            }
+        }
+
+        return minasCercanas;
+    }
+    public boolean esVictoria() {
+        int casillasRestantes = FILAS * COLUMNAS - MINAS;
+
+        for (int i = 0; i < FILAS; i++) {
+            for (int j = 0; j < COLUMNAS; j++) {
+                if (descubiertas[i][j]) {
+                    casillasRestantes--;
+                }
+            }
+        }
+
+        return casillasRestantes == 0;
+    }
 }
--- src/Principal/InterfaceConsole.java
+++ src/Principal/InterfaceConsole.java
@@ -1,24 +1,81 @@
 package Principal;
+import BuscaMinas.BuscaMinas;
+
+import java.util.Scanner;
 
 public class InterfaceConsole {
-    public void presentacion(){
-        System.out.println("Buenos días y bienvenidos!");
-        System.out.println("Este juego es el Busca Minas!");
-        System.out.println("EL TABLERO ES ASI: ");
-        System.out.println("  | 1 | | 2 | | 3 | | 4 | | 5 | | 6 | | 7 | | 8 | ");
-        System.out.println("1 |   | |   | |   | |   | |   | |   | |   | |   | ");
-        System.out.println("2 |   | |   | |   | |   | |   | |   | |   | |   | ");
-        System.out.println("3 |   | |   | |   | |   | |   | |   | |   | |   | ");
-        System.out.println("4 |   | |   | |   | |   | |   | |   | |   | |   | ");
-        System.out.println("5 |   | |   | |   | |   | |   | |   | |   | |   | ");
-        System.out.println("6 |   | |   | |   | |   | |   | |   | |   | |   | ");
-        System.out.println("7 |   | |   | |   | |   | |   | |   | |   | |   | ");
-        System.out.println("8 |   | |   | |   | |   | |   | |   | |   | |   | ");
-        System.out.println("Es tan senncillo como indicar donde quieres pinchar con una orden pEj.: 25 indicando linea y columna");
-        System.out.println("A jugaaar!!");
-    }
+    BuscaMinas juego = new BuscaMinas();
+
     public static void main(String[] args) {
-        InterfaceConsole jogo = new InterfaceConsole();
-        jogo.presentacion();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Bienvenido al Buscaminas");
+
+        InterfaceConsole interfaz = new InterfaceConsole();
+
+        interfaz.juego.inicializarTablero();
+        interfaz.juego.colocarMinas();
+        interfaz.imprimirTablero();
+
+        while (true) {
+            System.out.print("Introduce la posicion ('fila columna' con un espacio): ");
+            int fila = sc.nextInt() - 1;
+            int columna = sc.nextInt() - 1;
+
+            if (interfaz.juego.esMina(fila, columna)) {
+                System.out.println("DIABLOSSSSSS... EXPLOTOOOOOUUUUU");
+                interfaz.mostrarMinas();
+                break;
+            }
+
+            interfaz.juego.descubrirCasilla(fila, columna);
+            interfaz.imprimirTablero();
+
+            if (interfaz.juego.esVictoria()) {
+                System.out.println("Oleeeee LO HAS CONSEGUIDOOOO");
+                break;
+            }
+        }
+
+        sc.close();
+    }
+    public void imprimirTablero() {
+        System.out.println("  | 1 2 3 4 5 6 7 8");
+        System.out.println("--|----------------");
+
+        for (int i = 0; i < 8; i++) {
+            System.out.print((i + 1) + " | ");
+
+            for (int j = 0; j < 8; j++) {
+                if (juego.descubiertas[i][j]) {
+                    if (juego.minas[i][j]) {
+                        System.out.print("* ");
+                    } else {
+                        int minasCercanas = juego.contarMinasCercanas(i, j);
+                        System.out.print(minasCercanas + " ");
+                    }
+                } else {
+                    System.out.print(juego.tablero[i][j] + " ");
+                }
+            }
+
+            System.out.println();
+        }
+
+        System.out.println();
+    }
+    private void mostrarMinas() {
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (juego.minas[i][j]) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print(". ");
+                }
+            }
+
+            System.out.println();
+        }
+
+        System.out.println();
     }
 }
