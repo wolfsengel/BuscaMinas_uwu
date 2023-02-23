30c0b53e06ee8124d3ff3785945a297f8b6cc835
angelgp <a22angelgp@iessanclemente.net>
2023-02-23 10:49:42 +0100

    Initial 2

--- JAVADOC/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"BuscaMinas","l":"BuscaMinas"},{"p":"Principal","l":"InterfaceConsole"}];updateSearchResults();
